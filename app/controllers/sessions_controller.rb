a5f5def0fc8c1056765251d365dfb27870baefc1
miyata-t <miyata382tutomu@gmail.com>
2024-04-25 00:10:53 +0900

    add: ログインのバリデーション処理とエラーメッセージの表示の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,33 @@ class SessionsController < ApplicationController
   def new;end
 
   def create
-    redirect_to photos_path
+    user_name = login_params[:name]
+    password = login_params[:password]
+
+    @errors = []
+    @errors << "ユーザーIDが未入力です" if user_name.blank?
+    @errors << "パスワードが未入力です" if password.blank?
+
+    return render :new, status: :unprocessable_entity if @errors.present?
+
+    begin
+      user = User.find_by!(name: user_name)
+    rescue ActiveRecord::RecordNotFound
+      @errors << "ユーザーIDもしくはパスワードが誤っています"
+      return render :new, status: :unprocessable_entity
+    end
+
+    if user && user.authenticate(password)
+      redirect_to photos_path
+    else
+      @errors << "ユーザーIDもしくはパスワードが誤っています"
+      return render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def login_params
+    params.permit(:name, :password)
   end
 end
