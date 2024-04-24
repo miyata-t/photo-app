7a02c5d99eca07a5dbd2ce1c749ea6b1ffbbf403
miyata-t <miyata382tutomu@gmail.com>
2024-04-24 22:35:16 +0900

    add: ログイン後、写真一覧画面に遷移できるところまでを実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,7 @@
 class SessionsController < ApplicationController
   def new;end
+
+  def create
+    redirect_to photos_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  resources :photos, only: %i[index]
 end
