edb9344f83f4dd06220695822d09a8a15df4ea65
miyata-t <miyata382tutomu@gmail.com>
2024-04-25 18:42:06 +0900

    add: rootにアクセスされたらログイン画面に遷移させる

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'sessions#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   resources :photos, only: %i[index]
