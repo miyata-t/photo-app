eb246d3f0c6e057af4aa5c2ecdc59be75d96fb10
miyata-t <miyata382tutomu@gmail.com>
2024-04-26 15:22:35 +0900

    add: ログインが必要な画面用のログインチェックメソッドを追加
    sessionにuser_idがあって、テーブルにない場合は不整合の状態なのでログを出す

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+
+  private
+
+  def require_login
+    redirect_to login_path unless login?
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,4 +2,15 @@ module SessionsHelper
   def login(user)
     session[:user_id] = user.id
   end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  rescue ActiveRecord::RecordNotFound => e
+    logger.error "ユーザーが見つかりませんでした。user_id: #{session[:user_id]}"
+    nil
+  end
+
+  def login?
+    current_user.present?
+  end
 end
