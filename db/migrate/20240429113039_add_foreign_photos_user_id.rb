47c68f405beb7d5ee96da2cb5cdd13c7842ec6a1
miyata-t <miyata382tutomu@gmail.com>
2024-04-29 20:59:33 +0900

    add: Photoテーブルに外部キーを追加

---
+++ db/migrate/20240429113039_add_foreign_photos_user_id.rb
@@ -0,0 +1,5 @@
+class AddForeignPhotosUserId < ActiveRecord::Migration[7.0]
+  def change
+    add_foreign_key :photos, :users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_26_102921) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_29_113039) do
   create_table "photos", force: :cascade do |t|
     t.integer "user_id"
     t.string "title", limit: 30
@@ -26,4 +26,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_26_102921) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "photos", "users"
 end
