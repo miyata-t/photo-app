07cc8ca6576cf704785ed4dcefe07902fe2710db
miyata-t <miyata382tutomu@gmail.com>
2024-04-29 21:58:59 +0900

    refactor: 画像のpathを返すメソッドをmodelに追加
    共通で使うことを考えた時にModelに持たせておくと汎用性があって良い

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,4 +4,8 @@ class Photo < ApplicationRecord
   validates :title, presence: { message: 'タイトルを入力してください。' },
                     length: { maximum: 30, too_long: 'タイトルは最大%<count>s文字までです。' }
   validates :file_name, presence: { message: '画像ファイルを指定してください。' }
+
+  def image_path
+    "/photo/#{user_id}/#{file_name}"
+  end
 end
