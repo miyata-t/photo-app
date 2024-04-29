cbbf6bf97ddce965c6e381d0b6639acc39fe9308
miyata-t <miyata382tutomu@gmail.com>
2024-04-29 22:14:34 +0900

    add: ツイート成功失敗がわかるように画面にメッセージを出力する

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,10 +3,12 @@ class TweetsController < ApplicationController
     client = MyTweetHttpClient.new
     client.tweet_request(title: params[:title], image_url: params[:image_url], access_token: session[:access_token])
 
+    flash[:notice] = 'ツイートに成功しました。'
     redirect_to photos_path
   rescue MyTweetHttpClient::ClientError => e
     logger.error "error message: #{e.message}"
 
+    flash[:error] = 'ツイートに失敗しました。'
     redirect_to photos_path
   end
 end
