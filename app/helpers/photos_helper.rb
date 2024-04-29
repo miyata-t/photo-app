221a287e92739a799f098e5be0c4a0661cd14c50
miyata-t <miyata382tutomu@gmail.com>
2024-04-29 22:50:02 +0900

    remove: 不要になったメソッドを削除

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -4,8 +4,4 @@ module PhotosHelper
 
     "#{settings[:base_url]}#{settings[:authorization_path]}?response_type=code&client_id=#{settings[:client_id]}&redirect_uri=#{settings[:redirect_uri]}&scope=write_tweet"
   end
-
-  def local_image_url(path)
-    root_url + path
-  end
 end
