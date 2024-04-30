76f61f5b090478465983fb94e18d5b710e018dea
miyata-t <miyata382tutomu@gmail.com>
2024-04-30 18:34:58 +0900

    add: seed実行時にユーザーIDとパスワードを出力する

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
-User.create!(
+user = User.create!(
   name: "user_#{User.maximum(:id).to_i + 1}",
   password: "passw0rd"
 )
+
+puts "####作成したユーザー####"
+puts "ユーザーID: #{user.name}"
+puts "パスワード: passw0rd"
